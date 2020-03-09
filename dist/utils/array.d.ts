c276e198457feca95096f95fa2f19206afc10487
Scott Bedard <scottbedard90@gmail.com>
2020-03-08 21:37:12 -0700

    build

---
+++ dist/utils/array.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Rotate an array forwards or backwards. This mutates
+ * the original array.
+ *
+ * @param {any[]}   arr
+ * @param {number}  distance
+ *
+ * @return {void}
+ */
+export declare function roll(arr: any[], distance: number): void;
--- dist/utils/face.d.ts
+++ dist/utils/face.d.ts
@@ -2,9 +2,18 @@ import { Face } from '../types';
 /**
  * Create a turnable puzzle face.
  *
- * @param {number}      sides   number of sides of the polygon
- * @param {number}      layers  puzzle layers
+ * @param {number}  sides   number of sides of the polygon
+ * @param {number}  layers  puzzle layers
  *
  * @return {Face}
  */
 export declare function createFace(sides: number, layers: number): Face;
+/**
+ * Create a rotated copy of a face.
+ *
+ * @param {Face}    face
+ * @param {number}  rotation
+ *
+ * @return {Face}
+ */
+export declare function rotateFace(face: Face, rotation: number): Face;
