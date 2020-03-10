1f7a74cb00160730f1ed9c06298cd71dfbbf7cad
Scott Bedard <scottbedard90@gmail.com>
2020-03-09 17:20:08 -0700

    refactor roll to be clockwise and non-mutating

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,14 +1,13 @@
 /**
- * Rotate an array forwards or backwards. This mutates
- * the original array.
+ * Rotate an array forwards or backwards.
  *
  * @param {any[]}   arr 
- * @param {number}  distance
+ * @param {number}  elements
  *
- * @return {void} 
+ * @return {any[]} 
  */
-export function roll(arr: any[], distance: number): void {
-    distance -= arr.length * Math.floor(distance / arr.length);
-    
-    arr.push.apply(arr, arr.splice(0, distance));
+export function roll(arr: any[], elements: number) {
+    const offset = (((elements % arr.length) + arr.length) % arr.length);
+
+    return arr.slice(offset).concat(arr.slice(0, offset));
 }
---
+++ tests/utils/array.spec.ts
@@ -0,0 +1,28 @@
+import { roll } from '../../src/utils/array';
+
+describe('array utils', () => {
+    describe('roll', () => {
+        it('returns a new array', () => {
+            const original = [1, 2, 3];
+            const rolled = roll(original, 0);
+
+            expect(rolled).not.toBe(original);
+            expect(rolled).toEqual(original);
+            expect(rolled).toEqual([1, 2, 3]);
+        });
+
+        it('backwards', () => {
+            expect(roll([1, 2, 3], -1)).toEqual([3, 1, 2]);
+            expect(roll([1, 2, 3], -2)).toEqual([2, 3, 1]);
+            expect(roll([1, 2, 3], -3)).toEqual([1, 2, 3]);
+            expect(roll([1, 2, 3], -4)).toEqual([3, 1, 2]);
+        });
+
+        it('forwards', () => {
+            expect(roll([1, 2, 3], 1)).toEqual([2, 3, 1]);
+            expect(roll([1, 2, 3], 2)).toEqual([3, 1, 2]);
+            expect(roll([1, 2, 3], 3)).toEqual([1, 2, 3]);
+            expect(roll([1, 2, 3], 4)).toEqual([2, 3, 1]);
+        });
+    });
+});
