a2b7d90cae7f6fdd3480019f94a6d82cc692e376
Scott Bedard <scottbedard90@gmail.com>
2020-03-14 15:12:04 -0700

    remove old code

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,2 @@
-export {
-    createFace,
-    extractSlice,
-    rotateFace,
-} from './utils/face';
-
 export { default as Cube } from './cube/cube';
 export { default as Dodecaminx } from './dodecaminx/dodecaminx';
