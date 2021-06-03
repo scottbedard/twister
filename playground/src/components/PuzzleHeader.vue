134543285425a17f5e2f6d48c2f8e253251c2ea7
Scott Bedard <scottbedard90@gmail.com>
2021-06-02 22:20:15 -0500

    clean slate

--- playground/src/components/PuzzleHeader.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <h2 class="font-bold leading-none mb-6 text-2xl">
-    <slot />
-  </h2>
-</template>
--- playground/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  purge: [
-    './**/*.html',
-    './**/*.vue',
-  ],
-  plugins: [],
-  variants: {},
-};
