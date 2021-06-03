3dd11fead41f74afa63bfb9f2e9d52e0bd5fc210
Scott Bedard <scottbedard90@gmail.com>
2021-06-02 22:40:25 -0500

    configuring different builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,12 @@
+import { terser } from 'rollup-plugin-terser'
+import del from 'rollup-plugin-delete'
 import pkg from './package.json'
 import typescript from '@rollup/plugin-typescript'
 
 export default {
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+  ],
   input: 'src/index.ts',
   output: [
     {
@@ -16,9 +21,15 @@ export default {
       file: pkg.unpkg,
       format: 'iife',
       name: 'Twister',
+      plugins: [
+        terser(),
+      ],
     },
   ],
   plugins: [
-    typescript()
+    del({
+      targets: 'dist/*',
+    }),
+    typescript(),
   ],
 }
