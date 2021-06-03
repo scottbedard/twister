03397884f365e09af2cec4ebff39ebaed015f821
Scott Bedard <scottbedard90@gmail.com>
2021-06-02 23:43:16 -0500

    more eslint config

--- src/puzzle.ts
+++ src/puzzle.ts
@@ -1,18 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable class-methods-use-this */
 
 /**
  * Base puzzle class.
  */
-export class Puzzle {
+export abstract class Puzzle {
+  /**
+   * Generate a scramble.
+   *
+   * @param {number} depth number of moves to execute
+   */
+  generateScramble(depth: number): string {
+    return '';
+  }
+
   /**
    * Scramble the puzzle.
+   *
+   * @param {number} depth number of moves to execute
    */
-  scramble() { }
+  scramble(depth: number): void { }
 
   /**
    * Test if the puzzle is solved.
    */
-  test() {
+  test(): boolean {
     return true;
   }
 }
