c258c9280f64156c486fefec247883734495679f
Scott Bedard <scottbedard90@gmail.com>
2021-06-03 00:20:40 -0500

    fleshing out base class

--- src/puzzle.ts
+++ src/puzzle.ts
@@ -4,27 +4,81 @@
 /**
  * Base puzzle class.
  */
-export abstract class Puzzle {
+export abstract class Puzzle<Options, State, Turn> {
+  /**
+   * Puzzle options.
+   *
+   * @type {Options}
+   */
+  options: Options;
+
+  /**
+   * Current puzzle state.
+   *
+   * @type {State}
+   */
+  state: State;
+
+  /**
+   * Apply puzzle state.
+   *
+   * @param {any} state state to apply to the puzzle
+   */
+  abstract apply(state: State): void;
+
+  /**
+   * Constructor.
+   *
+   * @param {Options}  object
+   */
+  constructor(options: Options) {
+    this.options = options;
+
+    this.reset();
+  }
+
+  /**
+   * Execute a sequence of turns.
+   *
+   * @param {algorithm} string turns to execute
+   */
+  abstract execute(algorithm: string): void;
+
   /**
    * Generate a scramble.
    *
    * @param {number} depth number of moves to execute
    */
-  generateScramble(depth: number): string {
-    return '';
-  }
+  abstract generateScramble(depth: number): string;
+
+  /**
+   * Output puzzle state.
+   */
+  abstract output(): State;
+
+  /**
+   * Parse a turn.
+   *
+   * @param {string} turn Turn to parse
+   */
+  abstract parse(turn: string): Turn;
+
+  /**
+   * Reset puzzle state.
+   */
+  abstract reset(): void;
 
   /**
    * Scramble the puzzle.
    *
    * @param {number} depth number of moves to execute
    */
-  scramble(depth: number): void { }
+  scramble(depth: number): void {
+    this.execute(this.generateScramble(depth));
+  }
 
   /**
    * Test if the puzzle is solved.
    */
-  test(): boolean {
-    return true;
-  }
+  abstract test(): boolean;
 }
