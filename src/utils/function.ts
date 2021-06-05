d51e6d93804ccc7b13b1d5479d9f12dd3b5f2c6e
Scott Bedard <scottbedard90@gmail.com>
2021-06-05 12:05:23 -0500

    identity

--- src/utils/function.ts
+++ src/utils/function.ts
@@ -6,3 +6,12 @@
 export function error(message?: string) {
   throw new Error(message);
 }
+
+/**
+ * This method returns the first argument it receives.
+ *
+ * @param {T} arg Value to return.
+ */
+export function identity<T>(arg: T) {
+  return arg;
+}
--- tests/utils/function.spec.ts
+++ tests/utils/function.spec.ts
@@ -1,7 +1,16 @@
-import { error } from '@/utils/function';
+import {
+  error,
+  identity,
+} from '@/utils/function';
 
 describe('function utils', () => {
   it('error', () => {
     expect(() => error()).toThrow();
+  });
+
+  it('identity', () => {
+    const obj = {};
+
+    expect(identity(obj)).toBe(obj);
   });
 });
