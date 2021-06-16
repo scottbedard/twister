940cdad43ba6daac7adc202a5b08b17a983a73eb
Scott Bedard <scottbedard90@gmail.com>
2021-06-16 00:42:36 -0500

    dodecaminx U rotations

--- tests/puzzles/dodecaminx-turns.ts
+++ tests/puzzles/dodecaminx-turns.ts
@@ -1,5 +1,5 @@
-import { DodecaminxStateSimple } from '@/puzzles/dodecaminx/types';
 import { Dodecaminx } from '@/index';
+import { DodecaminxStateSimple } from '@/puzzles/dodecaminx/types';
 
 const model = new Dodecaminx({ size: 2 });
 
@@ -23,17 +23,90 @@ type TurnTest = {
   turn: string,
 };
 
+//
+// megaminx
+//
+const stub5x3: DodecaminxStateSimple = {
+  u: [
+    [[u], [u], [u], [u], [u]],
+    [[u], [u], [u], [u], [u]],
+    u,
+  ],
+  l: [
+    [[l], [l], [l], [l], [l]],
+    [[l], [l], [l], [l], [l]],
+    l,
+  ],
+  f: [
+    [[f], [f], [f], [f], [f]],
+    [[f], [f], [f], [f], [f]],
+    f,
+  ],
+  r: [
+    [[r], [r], [r], [r], [r]],
+    [[r], [r], [r], [r], [r]],
+    r,
+  ],
+  bl: [
+    [[bl], [bl], [bl], [bl], [bl]],
+    [[bl], [bl], [bl], [bl], [bl]],
+    bl,
+  ],
+  br: [
+    [[br], [br], [br], [br], [br]],
+    [[br], [br], [br], [br], [br]],
+    br,
+  ],
+  dl: [
+    [[dl], [dl], [dl], [dl], [dl]],
+    [[dl], [dl], [dl], [dl], [dl]],
+    dl,
+  ],
+  dr: [
+    [[dr], [dr], [dr], [dr], [dr]],
+    [[dr], [dr], [dr], [dr], [dr]],
+    dr,
+  ],
+  dbl: [
+    [[dbl], [dbl], [dbl], [dbl], [dbl]],
+    [[dbl], [dbl], [dbl], [dbl], [dbl]],
+    dbl,
+  ],
+  dbr: [
+    [[dbr], [dbr], [dbr], [dbr], [dbr]],
+    [[dbr], [dbr], [dbr], [dbr], [dbr]],
+    dbr,
+  ],
+  b: [
+    [[b], [b], [b], [b], [b]],
+    [[b], [b], [b], [b], [b]],
+    b,
+  ],
+  d: [
+    [[d], [d], [d], [d], [d]],
+    [[d], [d], [d], [d], [d]],
+    d,
+  ],
+};
+
+//
+// tests
+//
 const tests: TurnTest[] = [
+  // {
+  //   only: false,
+  //   size: 3,
+  //   turn: '',
+  //   result: {
+  //     ...stub5x3,
+  //   },
+  // },
   {
     only: false,
     size: 3,
     turn: 'U',
     result: {
-      u: [
-        [[u], [u], [u], [u], [u]],
-        [[u], [u], [u], [u], [u]],
-        u,
-      ],
+      ...stub5x3,
       l: [
         [[f], [f], [l], [l], [l]],
         [[f], [l], [l], [l], [l]],
@@ -59,35 +132,104 @@ const tests: TurnTest[] = [
         [[br], [br], [bl], [br], [br]],
         br,
       ],
-      dl: [
-        [[dl], [dl], [dl], [dl], [dl]],
-        [[dl], [dl], [dl], [dl], [dl]],
-        dl,
+    },
+  },
+  {
+    only: false,
+    size: 3,
+    turn: 'U-',
+    result: {
+      ...stub5x3,
+      l: [
+        [[bl], [bl], [l], [l], [l]],
+        [[bl], [l], [l], [l], [l]],
+        l,
       ],
-      dr: [
-        [[dr], [dr], [dr], [dr], [dr]],
-        [[dr], [dr], [dr], [dr], [dr]],
-        dr,
+      f: [
+        [[l], [f], [f], [f], [l]],
+        [[f], [f], [f], [f], [l]],
+        f,
       ],
-      dbl: [
-        [[dbl], [dbl], [dbl], [dbl], [dbl]],
-        [[dbl], [dbl], [dbl], [dbl], [dbl]],
-        dbl,
+      r: [
+        [[r], [r], [r], [f], [f]],
+        [[r], [r], [r], [f], [r]],
+        r,
       ],
-      dbr: [
-        [[dbr], [dbr], [dbr], [dbr], [dbr]],
-        [[dbr], [dbr], [dbr], [dbr], [dbr]],
-        dbr,
+      bl: [
+        [[bl], [br], [br], [bl], [bl]],
+        [[bl], [br], [bl], [bl], [bl]],
+        bl,
       ],
-      b: [
-        [[b], [b], [b], [b], [b]],
-        [[b], [b], [b], [b], [b]],
-        b,
+      br: [
+        [[br], [br], [r], [r], [br]],
+        [[br], [br], [r], [br], [br]],
+        br,
       ],
-      d: [
-        [[d], [d], [d], [d], [d]],
-        [[d], [d], [d], [d], [d]],
-        d,
+    },
+  },
+  {
+    only: false,
+    size: 3,
+    turn: 'U2',
+    result: {
+      ...stub5x3,
+      l: [
+        [[r], [r], [l], [l], [l]],
+        [[r], [l], [l], [l], [l]],
+        l,
+      ],
+      f: [
+        [[br], [f], [f], [f], [br]],
+        [[f], [f], [f], [f], [br]],
+        f,
+      ],
+      r: [
+        [[r], [r], [r], [bl], [bl]],
+        [[r], [r], [r], [bl], [r]],
+        r,
+      ],
+      bl: [
+        [[bl], [f], [f], [bl], [bl]],
+        [[bl], [f], [bl], [bl], [bl]],
+        bl,
+      ],
+      br: [
+        [[br], [br], [l], [l], [br]],
+        [[br], [br], [l], [br], [br]],
+        br,
+      ],
+    },
+  },
+  {
+    only: false,
+    size: 3,
+    turn: 'U2-',
+    result: {
+      ...stub5x3,
+      l: [
+        [[br], [br], [l], [l], [l]],
+        [[br], [l], [l], [l], [l]],
+        l,
+      ],
+      f: [
+        [[bl], [f], [f], [f], [bl]],
+        [[f], [f], [f], [f], [bl]],
+        f,
+      ],
+      r: [
+        [[r], [r], [r], [l], [l]],
+        [[r], [r], [r], [l], [r]],
+        r,
+      ],
+      bl: [
+        [[bl], [r], [r], [bl], [bl]],
+        [[bl], [r], [bl], [bl], [bl]],
+        bl,
+      ],
+      br: [
+        [[br], [br], [f], [f], [br]],
+        [[br], [br], [f], [br], [br]],
+        br,
       ],
     },
   },
