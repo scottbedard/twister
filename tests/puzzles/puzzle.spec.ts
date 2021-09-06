27579f24ab54ec7379a433a1ee633df0329eed81
Scott Bedard <scottbedard90@gmail.com>
2021-09-06 13:02:53 -0500

    cover base puzzle methods

---
+++ tests/puzzles/puzzle.spec.ts
@@ -0,0 +1,69 @@
+import { Puzzle } from '@/index';
+
+describe('Puzzle', () => {
+  it('apply', () => {
+    const puzzle = new Puzzle();
+
+    expect(() => puzzle.apply({})).not.toThrow();
+  });
+
+  it('execute', () => {
+    const puzzle = new Puzzle();
+
+    expect(() => puzzle.execute({})).not.toThrow();
+  });
+
+  it('generateScramble', () => {
+    const puzzle = new Puzzle();
+
+    expect(puzzle.generateScramble()).toEqual('');
+  });
+
+  it('output', () => {
+    const puzzle = new Puzzle();
+
+    expect(puzzle.output()).toEqual({});
+  });
+
+  it('parse', () => {
+    const puzzle = new Puzzle();
+
+    expect(puzzle.parse('')).toEqual({});
+  });
+
+  it('parseAlgorithm', () => {
+    const puzzle = new Puzzle();
+
+    expect(puzzle.parseAlgorithm('')).toEqual([]);
+  });
+
+  it('reset', () => {
+    const puzzle = new Puzzle();
+
+    expect(() => puzzle.reset()).not.toThrow();
+  });
+
+  it('scramble', () => {
+    const puzzle = new Puzzle();
+
+    expect(puzzle.scramble()).toEqual('');
+  });
+
+  it('stickers', () => {
+    const puzzle = new Puzzle();
+
+    expect(puzzle.stickers()).toEqual([]);
+  });
+
+  it('test', () => {
+    const puzzle = new Puzzle();
+
+    expect(puzzle.test()).toBe(true);
+  });
+
+  it('turn', () => {
+    const puzzle = new Puzzle();
+
+    expect(() => puzzle.turn('')).not.toThrow();
+  });
+});
