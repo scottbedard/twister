6ba4de3cd5eeba24c1b83e3d595c425c86d79bb7
Scott Bedard <scottbedard90@gmail.com>
2026-03-01 12:31:40 -0600

    types

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
+import dts from 'vite-plugin-dts'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
+  plugins: [
+    dts({
+      include: ['src'],
+      outDir: 'dist',
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
