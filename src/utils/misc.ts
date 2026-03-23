a4fe1b63f06a5f32ef0b12c8fac63f30c0bc2c40
Scott Bedard <scottbedard90@gmail.com>
2026-03-22 21:33:59 -0500

    dead code

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -20,13 +20,6 @@ export function keys<T extends Record<string, unknown>>(obj: T) {
 }
 
 /**
- * Returns the larger of a set of supplied numeric expressions.
- */
-export function max(...values: number[]): number {
-  return Math.max(...values)
-}
-
-/**
  * Returns the smaller of a set of supplied numeric expressions.
  */
 export function min(...values: number[]): number {
