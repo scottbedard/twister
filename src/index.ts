6f5b3ea8b4512b5c6be9a558cfd4067864f9c284
Scott Bedard <scottbedard90@gmail.com>
2026-04-15 21:16:00 -0500

    export additional types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 export { Cube } from './cube/index'
+
+export type {
+  CubeFace,
+  CubeSticker,
+  CubeTurn,
+} from './cube/types'
+
 export { Dodecaminx } from './dodecaminx/index'
+
+export type {
+  DodecaminxFace,
+  DodecaminxSticker,
+  DodecaminxTurn,
+} from './dodecaminx/types'
+
 export type { Puzzle } from './puzzle'
